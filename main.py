91c190f3badd79293053037f63a12a830f9ef8f7
EVD <v.emeljanova-27@yandex.ru>
2021-12-27 22:23:44 +0400

    ДЗ1

--- main.py
+++ main.py
@@ -1,16 +1,93 @@
-# This is a sample Python script.
+#1. Реализовать вывод информации о промежутке времени в зависимости от его продолжительности duration в секундах: до минуты: <s> сек; до часа: <m> мин <s> сек; до суток: <h> час <m> мин <s> сек; * в остальных случаях: <d> дн <h> час <m> мин <s> сек.
+#Примеры:
+#
+#duration = 53
+#53 сек
+#2 мин 33 сек
+#duration = 4153
+#1 час 9 мин 13 сек
+#duration = 400153
+#4 дн 15 час 9 мин 13 сек
+#
+#Примечание: можете проверить себя здесь, подумайте, можно ли использовать цикл для проверки работы кода сразу для нескольких значений продолжительности, будет ли тут полезен список?
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#t = int(input('Введи число: '))
+#d, h, m, s = t // 86400, t // 3600, t // 60 % 60, t % 60
+#print(f'{d} дней {h} час {m} мин {s} cек')
+
+# Создать список, состоящий из кубов нечётных чисел от 1 до 1000 (куб X - третья степень числа X):
+# a. Вычислить сумму тех чисел из этого списка, сумма цифр которых делится нацело на 7. Например, число «19 ^ 3 = 6859» будем включать в сумму, так как 6 + 8 + 5 + 9 = 28 – делится нацело на 7. Внимание: использовать только арифметические операции!
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+cubes = [x**3 for x in range (1000) if  x%2 != 0 ]
+print('Cписок кубов нечётных чисел {}'.format(cubes))
+my_numbers_sum = 0
+my_numbers_sum_list=[]
+
+for i in range(len(cubes)):
+
+    my_str = str(cubes[i])
+    my_list = list(my_str)
+
+    for i in range(len(my_list)):
+        my_list[i] = int(my_list[i])
+
+    my_numbers_sum = sum(my_list)
+    print(my_numbers_sum)
+
+    if my_numbers_sum % 7 == 0:
+
+        print('Cумму чисел, делящихся на 7 : ', my_numbers_sum)
+        my_numbers_sum_list.append(my_numbers_sum)
+
+print('Список чисел, делящихся на 7 (задание 1) : ',my_numbers_sum_list)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# b.  К каждому элементу списка добавить 17 и заново вычислить сумму тех чисел из этого списка, сумма цифр которых делится нацело на 7.
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+cubes = [(x**3)+17 for x in range(100) if x%2 == 0]
+print('Cоздан список кубов нечётных чисел {}'.format(cubes))
+my_numbers_sum = 0
+my_numbers_sum_list_even_numbers =[]
+
+
+for i in range(len(cubes)):
+
+    my_str = str(cubes[i])
+    my_list = list(my_str)
+    #print('print my_list', my_list)
+    for i in range(len(my_list)):
+        my_list[i] = int(my_list[i])
+
+    for i in range(len(my_list)):
+        my_numbers_sum = my_numbers_sum + my_list[i]
+
+    if my_numbers_sum % 7 == 0:
+        print('Cумму чисел, делящихся на 7 : ',my_numbers_sum)
+        my_numbers_sum_list_even_numbers.append(my_numbers_sum)
+
+print('Список чисел, делящихся на 7 (задание 2) : ',my_numbers_sum_list_even_numbers)
+
+# 3.Склонение слова
+# Реализовать склонение слова «процент» во фразе «N процентов». Вывести эту фразу на экран отдельной строкой для каждого из чисел в интервале от 1 до 100:
+# 1 процент
+# 2 процента
+# 3 процента
+# 4 процента
+# 5 процентов
+# 6 процентов
+# ...
+# 100 процентов
+
+for i in range(100):
+    new_str=str(i+1)
+    new_list = list(new_str)
+    if int(new_list[-1])==1 and i+1!=11:
+        print('{} процент'.format(i + 1))
+    elif int(new_list[-1])>1 and int(new_list[-1])<= 4:
+        if  i+1> 10 and i+1<= 14:
+            print('{} процентов'.format(i + 1))
+        else:
+            print('{} процента'.format(i + 1))
+    else:
+        print('{} процентов'.format(i + 1))
