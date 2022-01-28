53e9a10bf7bbaa92f0d879be3741b4a6e837f213
EVD11 <92547469+EVD11@users.noreply.github.com>
2022-01-28 21:05:40 +0400

    Add files via upload

---
+++ task_6_6_add_price.py
@@ -0,0 +1,7 @@
+import sys
+
+    input_prices = list(map(lambda y:  f"{float(y):100.2f}", filter(
+        lambda x: x.replace(".", "").isdigit(), sys.argv[1:])))
+
+    with open(PRICE_FILE, "a", encoding="utf-8") as price_list:
+        print(*input_prices, sep="\n", file=price_list)
